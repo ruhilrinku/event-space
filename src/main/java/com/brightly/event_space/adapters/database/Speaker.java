c7acf9aef16101d28ff3e714443794390d021e28
Kumar <nishant.kumar@brightlysoftware.com>
2024-09-23 18:53:03 +0530

    added new entities

---
+++ src/main/java/com/brightly/event_space/adapters/database/Speaker.java
@@ -0,0 +1,40 @@
+package com.brightly.event_space.adapters.database;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.id.UUIDGenerator;
+
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "speaker", uniqueConstraints = {@UniqueConstraint(name = "UniqueSpeakerEmailAndTenant",
+        columnNames = {"tenantId", "email"})})
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Speaker implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "uuid-generator", strategy = GenerationType.IDENTITY)
+    @GenericGenerator(name = "uuid-generator", type = UUIDGenerator.class)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    private String name;
+    private String email;
+    private String link;
+    private String avatar;
+    private String description;
+    private UUID eventId;
+    private OffsetDateTime createDate;
+    private OffsetDateTime updatedDate;
+    private String tenantId;
+
+    @Version
+    private long version;
+}
